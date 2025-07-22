33cedad11c5c0d0cb0966de9f41d4aa69999ea9a
IAmVeryNewToThisWebsite <fofoangel858@gmail.com>
2025-07-21 22:04:21 -0500

    Add files via upload

---
+++ refrence sheet.py
@@ -0,0 +1,62 @@
+def placeholder():
+    print("Placeholder")
+
+def css():
+    print("Unfinished.")
+
+def html():
+    print("unfinished")
+
+def python():
+    print("Unlisted")
+
+def luau():
+    print('basic Luau:')
+    print("-----------------------------------------------------------------------------------------------------------------------------------------------")
+    print("print('Hello World!') -- prints anything inside the ('').")
+    print("Comments are made by using --. use --[[ ]]-- to use comments in multiple lines. --[[ is the start and ]]-- is the end.")
+    print()
+    print("'This is a string. It can store anything. just make sure to use quotations'")
+    print('59 -- This is a number/Int.')
+    print('32.7 --This is a float.')
+    print("nil --Nothing, mostly used for placeholders.")
+    print("(Make sure these data types are inside something like print())")
+    print()
+    print('local nameHere = 67 --Using local for variables is optional. Put any name where "nameHere" is. Add an = sign then use any data type.')
+    print()
+    print('baseplate = game.workspace.Baseplate --Locating always uses game at the start. If An object is inside an folder use a dot like this example.')
+    print('baseplate.Transparency = 1 --You can change its properties my locating it first then calling its property and using its data type after the =.')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("CSS")
+print("HTML")
+print("Luau")
+User_input = input("Please select a reference sheet shown.")
+
+if User_input.lower() == "css":
+    placeholder()
+
+if User_input.lower() == "html":
+    placeholder()
+
+if User_input.lower() == "python":
+    placeholder()
+
+if User_input.lower() == "luau":
+    luau()
